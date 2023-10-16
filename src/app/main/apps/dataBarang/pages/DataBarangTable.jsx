c86480b4a83d7ce9f9ee9566d44b80bfa57c85f6
mmaulana1999 <maulanamuhamad593@gmail.com>
2023-10-16 21:19:15 +0700

    <fix>(common): fix api data barang crud

--- src/app/main/apps/dataBarang/pages/DataBarangTable.jsx
+++ src/app/main/apps/dataBarang/pages/DataBarangTable.jsx
@@ -1,5 +1,251 @@
-function DataBarangTable() {
-  return <div>DataBarangTable</div>;
+/* eslint-disable react-hooks/exhaustive-deps */
+import * as React from 'react';
+import Paper from '@mui/material/Paper';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TablePagination from '@mui/material/TablePagination';
+import TableRow from '@mui/material/TableRow';
+import { showMessage } from 'app/store/fuse/messageSlice';
+import { useDispatch } from 'react-redux';
+import axios from 'axios';
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
+import { IconButton } from '@mui/material';
+import FuseLoading from '@fuse/core/FuseLoading';
+
+const columns = [
+  { id: 'no', label: 'NO', minWidth: 170, align: 'left' },
+  {
+    id: 'kodeBarang',
+    label: 'Kode Barang',
+    minWidth: 170,
+    align: 'center',
+    // format: (value) => value.toLocaleString('en-US'),
+  },
+  {
+    id: 'namaBarang',
+    label: 'Nama Barang',
+    minWidth: 170,
+    align: 'center',
+    // format: (value) => value.toLocaleString('en-US'),
+  },
+  {
+    id: 'hargaBarang',
+    label: 'Harga Barang',
+    minWidth: 170,
+    align: 'center',
+    // format: (value) => value.toLocaleString('en-US'),
+  },
+  {
+    id: 'aksi',
+    label: 'Aksi',
+    minWidth: 170,
+    align: 'right',
+    // format: (value) => value.toFixed(2),
+  },
+];
+
+function createData(no, id, kodeBarang, namaBarang, hargaBarang) {
+  return { no, id, kodeBarang, namaBarang, hargaBarang };
 }
 
-export default DataBarangTable;
+export default function DataBarangTable(props) {
+  // console.log(props, 'pp')
+  const dispatch = useDispatch();
+  const [data, setData] = React.useState([]);
+  const [loading, setLoading] = React.useState(true);
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+  const api = `https://652d2c32f9afa8ef4b26e7f0.mockapi.io/tokoBangunan/v1/tokoBangunan`;
+  const rows = props?.data?.map((item, index) =>
+    createData(index + 1, item?.id, item?.kodeBarang, item?.namaBarang, item?.hargaBarang)
+  );
+
+  const HandelEdit = () => {
+    setLoading(true);
+    axios
+      .put(`${api}`)
+      .then((res) => {
+        // setData(res?.data);
+        setLoading(false);
+        dispatch(
+          showMessage({
+            message: 'Data Berhasil Di Edit',
+            autoHideDuration: 2000,
+            anchorOrigin: {
+              vertical: 'top',
+              horizontal: 'center',
+            },
+            variant: 'error',
+          })
+        );
+      })
+      .catch((err) => {
+        setData([]);
+        setLoading(false);
+        const errStatus = err.response.status;
+        const errMessage = err.response.data.message;
+        let messages = '';
+        if (errStatus === 401) {
+          messages = 'Unauthorized!!';
+          window.location.href = '/login';
+        } else if (errStatus === 500) {
+          messages = 'Server Error!!';
+        } else if (errStatus === 404) {
+          messages = 'Not Found Error!!!';
+        } else if (errStatus === 408) {
+          messages = 'TimeOut Error!!';
+        } else if (errStatus === 400) {
+          messages = errMessage;
+        } else {
+          messages = 'Something Wrong!!';
+        }
+        dispatch(
+          showMessage({
+            message: messages,
+            autoHideDuration: 2000,
+            anchorOrigin: {
+              vertical: 'top',
+              horizontal: 'center',
+            },
+            variant: 'error',
+          })
+        );
+        console.log(err);
+      });
+  };
+  const HandelDelete = (id) => {
+    setLoading(true);
+    axios
+      .delete(`${api}/${id}`)
+      .then((res) => {
+        props?.getData();
+        setLoading(false);
+        dispatch(
+          showMessage({
+            message: 'Data Berhasil Di Hapus',
+            autoHideDuration: 2000,
+            anchorOrigin: {
+              vertical: 'top',
+              horizontal: 'center',
+            },
+            variant: 'success',
+          })
+        );
+      })
+      .catch((err) => {
+        setData([]);
+        setLoading(false);
+        const errStatus = err.response.status;
+        const errMessage = err.response.data.message;
+        let messages = '';
+        if (errStatus === 401) {
+          messages = 'Unauthorized!!';
+          window.location.href = '/login';
+        } else if (errStatus === 500) {
+          messages = 'Server Error!!';
+        } else if (errStatus === 404) {
+          messages = 'Not Found Error!!!';
+        } else if (errStatus === 408) {
+          messages = 'TimeOut Error!!';
+        } else if (errStatus === 400) {
+          messages = errMessage;
+        } else {
+          messages = 'Something Wrong!!';
+        }
+        dispatch(
+          showMessage({
+            message: messages,
+            autoHideDuration: 2000,
+            anchorOrigin: {
+              vertical: 'top',
+              horizontal: 'center',
+            },
+            variant: 'error',
+          })
+        );
+        console.log(err);
+      });
+  };
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
+  if (props?.loading) {
+    return <FuseLoading />;
+  }
+
+  return (
+    <Paper sx={{ width: '100%', overflow: 'hidden' }}>
+      <TableContainer sx={{ maxHeight: 440 }}>
+        <Table stickyHeader aria-label="sticky table">
+          <TableHead>
+            <TableRow>
+              {columns.map((column) => (
+                <TableCell
+                  key={column.id}
+                  align={column.align}
+                  style={{ minWidth: column.minWidth }}
+                >
+                  {column.label}
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows?.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row, index) => {
+              // console.log(row, 'oo');
+              return (
+                <TableRow key={row.id} hover role="checkbox" tabIndex={-1}>
+                  <TableCell>{index + 1}</TableCell>
+                  <TableCell>{row.kodeBarang}</TableCell>
+                  <TableCell>{row.namaBarang}</TableCell>
+                  <TableCell>{row.hargaBarang}</TableCell>
+                  <TableCell>
+                    <div className="flex justify-center">
+                      <div>
+                        <IconButton
+                          onClick={() => HandelEdit(row.id, row)}
+                          color="info"
+                          className=""
+                        >
+                          <EditIcon />
+                        </IconButton>
+                      </div>
+                      <div>
+                        <IconButton
+                          onClick={(e) => HandelDelete(row.id)}
+                          color="error"
+                          className=""
+                        >
+                          <DeleteIcon />
+                        </IconButton>
+                      </div>
+                    </div>
+                  </TableCell>
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[5, 10, 25, 100]}
+        component="div"
+        count={rows.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
+    </Paper>
+  );
+}
